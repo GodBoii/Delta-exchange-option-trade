26638614d40d06987be825fc89b7398432bf6398
Prajwal Ghadge <prajwalghadge2005@gmail.com>
2026-08-12 15:54:08 +0530

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -8,7 +8,7 @@ const compat = new FlatCompat({ baseDirectory: __dirname });
 
 const config = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
-  { ignores: [".next/**", "node_modules/**", "coverage/**", "next-env.d.ts"] }
+  { ignores: [".next/**", "node_modules/**", "coverage/**", "reference/**", "next-env.d.ts"] }
 ];
 
 export default config;
